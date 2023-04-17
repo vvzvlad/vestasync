68a13a620de94677073bcddf36bef8093178108b
vvzvlad <git@vvzvlad.xyz>
2023-04-17 15:21:53 +0700

    date format update

--- files/pushgit_inotify.sh
+++ files/pushgit_inotify.sh
@@ -6,8 +6,9 @@ do
     echo $(hostname) > /mnt/data/etc/vestasync/hostname
     export GIT_AUTHOR_NAME="vestasync_wb_$(hostname)_inotify"
     export GIT_COMMITTER_NAME="vestasync_wb_$(hostname)_inotify"
+    export LC_TIME=en_GB.UTF-8
     cd /mnt/data/etc/
     git add .
-    git commit -m "$(date)"
+    git commit -m "$(date +"%Y-%m-%d %H:%M:%S %z %Z")" # > /dev/null 2>&1 || true
     git push -u origin master
 done
